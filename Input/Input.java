63b8446567ef499246d02b6760f403ad96719128
iprash155 <prashantprajapati159@gmail.com>
2021-09-26 20:32:14 +0530

    file modified

--- Input/Input.java
+++ Input/Input.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class Input{
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-                System.out.println(input.nextLine());
-            
-        }
-        
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your name : ");
+        String name = input.nextLine();
+        System.out.println("hi "+name);
+    }
 }
+        
