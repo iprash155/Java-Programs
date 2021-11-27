e458c8a3fba91179f389c0648966e67413fe07ac
iprash155 <prashantprajapati159@gmail.com>
2021-11-27 13:14:19 +0530

    maxofall.java added

--- Basics/LargestNo/MaxOfAll.java
+++ Basics/LargestNo/MaxOfAll.java
@@ -3,30 +3,28 @@ package Basics.LargestNo;
 import java.util.Scanner;
 
 /**
- * Take integer inputs till the user enters 0 and print the largest number from all.
+ * Take integer inputs till the user enters 0 and print the largest number from
+ * all.
  */
 public class MaxOfAll {
     public static void main(String[] args) {
-        Scanner in =new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("enter the nos");
-        int i=0;
-        int max=0;
-        int prev=0;
-        while (true) {
-            i=in.nextInt();
-           
-                if (i==0) {
+        int i = 0;
+        int max = 0;
 
-                     break;    
-                } 
-                else {
-                    if(i>prev){
-                     int temp=i;
-                     max=i;
-                     prev=temp;
-                    }  
+        while (true) {
+            i = in.nextInt();
+
+            if (i == 0) {
+
+                break;
+            } else {
+                if (i > max) {
+                    max = i;
                 }
+            }
         }
-        System.out.println("the max no is "+max);
+        System.out.println("the max no is " + max);
     }
 }
