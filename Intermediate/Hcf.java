87ea9d13787617dbf7dc887898fe32b81cd592cd
Prashant Prajapati <60150532+iprash155@users.noreply.github.com>
2022-07-29 18:05:50 +0530

    Update Hcf.java

--- Intermediate/Hcf.java
+++ Intermediate/Hcf.java
@@ -12,21 +12,28 @@ public class Hcf {
         int b = n2;
         int rem = n1%n2;
         if (n1>n2) {
-            while(rem!=0){
-            rem=n1%n2;
-             n1=n2;
-             n2=rem;
-             
-             
+            if(n1%n2==0){
+              System.out.println("hcf is "+n2);
             }
-        System.out.println("hcf is "+n1);
+            else{
+                while(rem!=0){
+                    rem=n1%n2;
+                     n1=n2;
+                     n2=rem;
+                    }
+              System.out.println("hcf is "+n1);
+            }
+            
         } else {
-            while(rem!=0){
+            if(n2%n1==0){
+                System.out.println("hcf is "+n1);
+            }
+            else{
+            }
+                while(rem!=0){
                 rem=n2%n1;
                 n2=n1;
                 n1=rem;
-               
-
                }
                System.out.println("hcf is "+n2);
 
